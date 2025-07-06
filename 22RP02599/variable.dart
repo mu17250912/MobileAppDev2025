62213a0ec85bc54199281ade078dee5043bb1030
muhirwa1mj <muhirwaj1@gmail.com>
2025-07-06 12:16:38 +0200

    these are  my dart files work

---
+++ 22RP02599/variable.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Declare a variable of type String
+  String name = 'Alice';
+
+  // Declare a variable of type int
+  int age = 25;
+
+  // Declare a variable of type double
+  double height = 5.7;
+
+  // Declare a variable of type bool
+  bool isStudent = true;
+
+  // Print the variables
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height feet');
+  print('Is a student? $isStudent');
+}
