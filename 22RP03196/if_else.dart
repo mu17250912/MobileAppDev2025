48ac5f89566807bae558778b1f1dec1ab9b477bb
NsengiyumvaTheophile <164474959+NsengiyumvaTheophile@users.noreply.github.com>
2025-07-06 00:17:35 -0800

    Update if_else.dart

--- 22RP03196/if_else.dart
+++ 22RP03196/if_else.dart
@@ -1,11 +1,11 @@
 void main() {
-  int pacifiqueScore = 85;
+  int theophile = 85;
 
-  if (pacifiqueScore >= 90) {
-    print('Pacifique got an A');
+  if (theophile >= 90) {
+    print('theophile got an A');
   } else if (pacifiqueScore >= 75) {
-    print('Pacifique got a B');
+    print('theophile got a B');
   } else {
-    print('Pacifique needs to study more');
+    print('theophile needs to study more');
   }
 }
