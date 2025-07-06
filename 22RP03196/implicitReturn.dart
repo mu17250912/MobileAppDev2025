d3541824e90e024de6e943e44109ac12c225cb32
NsengiyumvaTheophile <164474959+NsengiyumvaTheophile@users.noreply.github.com>
2025-07-06 00:18:19 -0800

    Update implicitReturn.dart

--- 22RP03196/implicitReturn.dart
+++ 22RP03196/implicitReturn.dart
@@ -1,9 +1,9 @@
 // Dart automatically knows this returns a String
 Breakfast() {
   if (DateTime.now().hour < 12) {
-    return 'Pacifique enjoys pancakes 🥞 and Coffe';
+    return 'theophile enjoys pancakes 🥞 and Coffe';
   }
-  return 'Pacifique prefers an omelette 🍳 now';
+  return 'theophile prefers an omelette 🍳 now';
 }
 
 void main() {
