316b339f2d7532349196dcb70f490e53e168b119
NsengiyumvaTheophile <164474959+NsengiyumvaTheophile@users.noreply.github.com>
2025-07-06 00:14:26 -0800

    Update for_loops.dart

--- 22RP03196/for_loops.dart
+++ 22RP03196/for_loops.dart
@@ -1,13 +1,13 @@
 void main() {
-  List<String> pacifiqueFriends = ['Clara', 'Jean', 'Pacifique'];
+  List<String> theophileFriends = ['Clara', 'Jean', 'emmy'];
 
   // For loop
-  for (int i = 0; i < pacifiqueFriends.length; i++) {
-    print('Friend: ${pacifiqueFriends[i]}');
+  for (int i = 0; i < theophileFriends.length; i++) {
+    print('Friend: ${theophileFriends[i]}');
   }
 
   // For-in loop
-  for (var friend in pacifiqueFriends) {
-    print('Pacifique\'s friend: $friend');
+  for (var friend in theophile) {
+    print('theophile\'s friend: $friend');
   }
 }
